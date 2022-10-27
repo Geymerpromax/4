6aa5340883ea9fd470f2267ab7abe2bba4aa5b04
Руслан <@DESKTOP-V4KRFG3>
2022-10-27 17:37:30 +0300

    Добавьте файлы проекта.

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -4,40 +4,118 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <random>
-#include <initializer_list>
 #include <ctime>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
     srand(time(0));
-    double NN[33], average = 0;
-    int i;
-    cout << "\tИсходный массив:\n\n";
-    for (i = 0; i <= 32; ++i)
+    /*
+    float X[7][8], max = -100;
+    int  i, nom_str, nom_stb, j;
+    cout << "МАТРИЦА:\n";
+    for (i = 0; i < 7; i++)
     {
-        NN[i] = (-500 + (rand() % 5000)) * 0.01; //массив вещественных чисел с диапазоном от -5 до 50
-        average += (NN[i] / 33);
-        if (i % 6 == 0 and i != 0)
+        for (j = 0; j < 8; j++)
         {
+            X[i][j] = -10 + rand() % 50;
+            cout << X[i][j] << " ";
+            if (X[i][j] > max)
+            {
+                max = X[i][j];
+                nom_str = i;
+                nom_stb = j;
+            }
             cout << "\n";
         }
-        cout << "\t" << NN[i] << "\t";
+        cout << "\nМаксимальное значение = " << max;
+        cout << "\nНомер строки = " << nom_str;
+        cout << "\nНомер столбца = " << nom_stb << "\n\n";
+
+
     }
-    cout << "\n\n\tCреднее арифметическое массива: " << average;
-    cout << "\n\n\tНовый массив:\n\n";
-    for (i = 0; i <= 32; ++i)
+    */
+    /*
+    int F[5][5], i, j, Sum = 0;
+    cout << "\nМАТРИЦА F:\n";
+    for (i = 0; i < 5; i++)
     {
-        if (i >= 0 and i < 10)
+        for (j = 0; j < 5; j++)
         {
-            NN[i] = average;
+            F[i][j] = -10 + rand() % 50;
+            if (i == j)
+            {
+                Sum += F[i][j];
+            }
+            cout << F[i][j] << "\t";
         }
-        if (i % 6 == 0 and i != 0)
-        {
-            cout << "\n";
-        }
-        cout << "\t" << NN[i] << "\t";
+        cout << "\n";
     }
-    
+    cout << "\nСреднее арифметическое = " << Sum / 5.;
+    */
+    /*
+    int  s[9][6], i, j, Pr;
+    cout << "\nИСХОДНАЯ МАТРИЦА S:\n";
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            s[i][j] = -10 + rand() % 50;
+            cout << s[i][j] << "\t";
+        }
+       cout << "\n";
+    }
+    for (i = 0; i < 9; i++)
+    {
+        Pr = 1;
+        for (j = 0; j < 5; j++)
+        {
+            if (s[i][j] % 2 == 0)
+            {
+                Pr *= s[i][j];
+            }
+        }
+        s[i][5] = Pr;
+    }
+   cout << "\nИЗМЕНЁННЫЙ МАССИВ S:\n\n";
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 6; j++)
+        {
+            cout << s[i][j] << "\t";
+        }
+       cout << "\n";
+    }
+    */
+    //Рассчитать среднее арифметическое элементов, расположенных выше главной диагонали вещественной матрицы Y[9][9]
+    /*
+    float Y[9][9], average = 0;
+    int i, j, num = 0;
+    cout << "\n\tИСХОДНАЯ МАТРИЦА Y:\n";
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 9; j++)
+        {
+            Y[i][j] = rand() % 10;
+            cout << setprecision(3) << setw(8) << Y[i][j];
+        }
+        cout << endl;
+    }
+    cout << "\n\t*************************************\n";
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 9; j++)
+        {
+            if (i <= j)
+            {
+                average += Y[i][j];
+                num += 1;
+            }
+        }
+    }
+    cout << "\t" << "Среднее арифметическое: " << average / num << endl;
+    */
+    //Задание 2
 }
